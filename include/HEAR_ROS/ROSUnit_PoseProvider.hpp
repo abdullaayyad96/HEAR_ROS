41f6446fb28ecc12e9d967b1eafcc3ae8048b302
HazemElrefaei <helrifaei@gmail.com>
2023-07-19 18:49:38 +0400

    Add Vision pose

--- include/HEAR_ROS/ROSUnit_PoseProvider.hpp
+++ include/HEAR_ROS/ROSUnit_PoseProvider.hpp
@@ -7,6 +7,7 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <geometry_msgs/QuaternionStamped.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 
@@ -22,26 +23,32 @@ namespace HEAR{
 class ROSUnit_PoseProvider{
 private:
     ros::NodeHandle nh_;
-    ros::Subscriber opti_sub, xsens_ori_sub, xsens_ang_vel_sub, xsens_free_acc_sub;
+    ros::Subscriber opti_sub, vision_sub, xsens_ori_sub, xsens_ang_vel_sub, xsens_free_acc_sub;
     ros::ServiceServer m_server;
     
     ExternalOutputPort<Vector3D<float>>* opti_pos_port;
     ExternalOutputPort<Vector3D<float>>* opti_vel_port;
     ExternalOutputPort<Vector3D<float>>* opti_ori_port;
+    ExternalOutputPort<Vector3D<float>>* vision_pos_port;
+    ExternalOutputPort<Vector3D<float>>* vision_vel_port;
+    ExternalOutputPort<Vector3D<float>>* vision_ori_port;
     ExternalOutputPort<Vector3D<float>>* imu_ori_port;
     ExternalOutputPort<Vector3D<float>>* imu_acc_port;
     ExternalOutputPort<Vector3D<float>>* imu_angular_rt_port;
     void callback_opti_pose(const geometry_msgs::PoseStamped::ConstPtr& );
+    void callback_vision_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& );
     void callback_ori(const geometry_msgs::QuaternionStamped::ConstPtr& );
     void callback_free_acc(const geometry_msgs::Vector3Stamped::ConstPtr& );
     void callback_angular_vel(const geometry_msgs::Vector3Stamped::ConstPtr&);
     bool srv_callback(hear_msgs::set_float::Request&, hear_msgs::set_float::Response&);
-    tf2::Matrix3x3 rot_offset;
-    tf2::Vector3 trans_offset;
+    tf2::Matrix3x3 rot_offset, rot_offset_vision;
+    tf2::Vector3 trans_offset, trans_offset_vision;
 
     tf2::Vector3 opti_pos, prev_pos, opti_vel, prev_diff, _hold;
-    ros::Time prevT;
+    tf2::Vector3 vision_pos, prev_pos_vision, vision_vel, prev_diff_vision, _hold_vision;
+    ros::Time prevT, prevT_vision;
     uint8_t first_read = 0;
+    uint8_t first_read_vision = 0;
     const float PEAK_THRESH = 0.35;
 
 public:
@@ -49,6 +56,7 @@ public:
     ROSUnit_PoseProvider(ros::NodeHandle& nh);
     ~ROSUnit_PoseProvider(){}
     std::vector<ExternalOutputPort<Vector3D<float>>*> registerOptiPose(std::string t_name);
+    std::vector<ExternalOutputPort<Vector3D<float>>*> registerVisionPose(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuOri(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuAngularRate(std::string t_name);
     ExternalOutputPort<Vector3D<float>>* registerImuAcceleration(std::string t_name);
--- src/ROSUnit_PoseProvider.cpp
+++ src/ROSUnit_PoseProvider.cpp
@@ -21,6 +21,20 @@ std::vector<ExternalOutputPort<Vector3D<float>>*> ROSUnit_PoseProvider::register
     return std::vector<ExternalOutputPort<Vector3D<float>>*>{opti_pos_port, opti_vel_port, opti_ori_port};
 }
 
+std::vector<ExternalOutputPort<Vector3D<float>>*> ROSUnit_PoseProvider::registerVisionPose(std::string t_name){
+    rot_offset_vision.setRPY(0.0, 0.0, M_PI/2.0);
+    trans_offset_vision.setZero();
+
+    vision_pos_port = new ExternalOutputPort<Vector3D<float>>(0);
+    vision_pos_port->write(Vector3D<float>(0,0,0));
+    vision_vel_port = new ExternalOutputPort<Vector3D<float>>(0);
+    vision_vel_port->write(Vector3D<float>(0,0,0));
+    vision_ori_port = new ExternalOutputPort<Vector3D<float>>(0);
+    vision_ori_port->write(Vector3D<float>(0,0,0));
+    vision_sub = nh_.subscribe(t_name, 10, &ROSUnit_PoseProvider::callback_vision_pose, this, ros::TransportHints().tcpNoDelay());
+    return std::vector<ExternalOutputPort<Vector3D<float>>*>{vision_pos_port, vision_vel_port, vision_ori_port};
+}
+
 ExternalOutputPort<Vector3D<float>>* ROSUnit_PoseProvider::registerImuOri(std::string t_name){
     imu_ori_port = new ExternalOutputPort<Vector3D<float>>(0);
     imu_ori_port->write(Vector3D<float>(0,0,0));
@@ -95,6 +109,56 @@ void ROSUnit_PoseProvider::callback_opti_pose(const geometry_msgs::PoseStamped::
     opti_pos_port->write(vec);
     opti_vel_port->write(Vector3D<float>(opti_vel.x(), opti_vel.y(), opti_vel.z()));
     opti_ori_port->write(vec_ori);
+}
+
+void ROSUnit_PoseProvider::callback_vision_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
+    
+    tf2::Vector3 vel;
+    auto pos = tf2::Vector3({msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z});
+    auto calib_pos = rot_offset_vision*pos - trans_offset_vision;
+
+    Vector3D<float> vec = {(float)calib_pos.x(), (float)calib_pos.y(), (float)calib_pos.z()};
+
+    auto R_mat = tf2::Matrix3x3(tf2::Quaternion(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w ));
+
+    R_mat = rot_offset_vision * R_mat * rot_offset_vision.transpose();
+    tf2Scalar yaw, pitch, roll;
+    R_mat.getEulerYPR(yaw, pitch, roll);
+
+    Vector3D<float> vec_ori = {(float)roll, (float)pitch, (float)yaw};
+
+    // velocity calculation
+    if(first_read_vision == 0){
+        first_read_vision = 1;
+        prevT_vision = msg->header.stamp;
+        prev_pos_vision = pos;
+        vel = tf2::Vector3(0, 0, 0);
+        prev_diff_vision = vel;
+    }else{
+        auto _dt = (msg->header.stamp - prevT_vision).toSec();
+        auto diff = (pos - prev_pos_vision)/_dt;
+        vel = diff;
+        if(first_read_vision == 1){
+            first_read_vision = 2;
+            prev_diff_vision = diff;
+        }
+        auto d_diff = diff - prev_diff;
+        if(abs(d_diff.x()) > PEAK_THRESH || abs(d_diff.y()) > PEAK_THRESH || abs(d_diff.z()) > PEAK_THRESH){
+            vel = _hold_vision;
+        }
+        else{
+            _hold_vision = diff;
+        }
+        prev_diff_vision = diff;
+        prev_pos_vision = pos;
+        prevT_vision = msg->header.stamp;
+    }
+    vision_vel = rot_offset_vision*vel;
+    ////////////////////////
+
+    vision_pos_port->write(vec);
+    vision_vel_port->write(Vector3D<float>(vision_vel.x(), vision_vel.y(), vision_vel.z()));
+    vision_ori_port->write(vec_ori);
 }
 
 void ROSUnit_PoseProvider::callback_ori(const geometry_msgs::QuaternionStamped::ConstPtr& msg){
